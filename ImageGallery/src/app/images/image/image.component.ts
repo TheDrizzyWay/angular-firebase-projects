06792200fc035f0245036602b92a10aa4d613f9c
Christopher Iliyas <christopher.iliyas@andela.com>
2019-09-13 12:03:00 +0100

    finish image preview functionality

--- ImageGallery/src/app/images/image/image.component.ts
+++ ImageGallery/src/app/images/image/image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-image',
@@ -8,15 +9,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ImageComponent implements OnInit {
   formTemplate: FormGroup;
-  constructor() {
+  imageSrc: string | SafeUrl;
+  selectedImage: any;
+
+  constructor(private sanitizer: DomSanitizer) {
     this.formTemplate = new FormGroup({
       caption: new FormControl(''),
       category: new FormControl(''),
       imageUrl: new FormControl('')
     });
+    this.imageSrc = '/assets/images/image_placeholder.png';
+    this.selectedImage = null;
   }
 
   ngOnInit() {
+  }
+
+  showPreview(event: any): void {
+    if (event.target.files[0]) {
+      this.imageSrc = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(event.target.files[0]));
+      this.selectedImage = event.target.files[0];
+    } else {
+      this.imageSrc = '/assets/images/image_placeholder.png';
+      this.selectedImage = null;
+    }
+  }
+
+  onSubmit() {
+
   }
 
 }
